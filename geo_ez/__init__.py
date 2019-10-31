fd7c129531ee118b61f92dba29e1c23bed0c0775
Amos Vryhof <amos@vryhofresearch.com>
2019-10-31 10:14:59 -0400

    Fix migration issue

--- geo_ez/__init__.py
+++ geo_ez/__init__.py
@@ -1,5 +1,5 @@
 """
 @copyright Copyright Amos Vryhof
 """
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 __name__ = "geo_ez"
--- geo_ez/migrations/0002_streetaddress.py
+++ geo_ez/migrations/0002_streetaddress.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('gis', '0001_initial'),
+        ('geo_ez', '0001_initial'),
     ]
 
     operations = [
